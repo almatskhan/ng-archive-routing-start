6eb7f327bd3b54dd821760739ec192f7065f2086
Almat Sailaukhan <almat.skhan@gmail.com>
2023-11-27 21:11:35 +0600

    archive

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+
+import { AuthService } from "./auth.service";
+
+@Injectable()
+export class AuthGuard implements CanActivate, CanActivateChild{
+    constructor(
+        private authService: AuthService,
+        private router: Router) {
+        
+    }
+
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ) : boolean | Observable<boolean> | Promise<boolean> {
+        return this.authService.isAuth()
+            .then(
+                (auth: boolean) => {
+                    if (auth) {
+                        return true
+                    } else {
+                        this.router.navigate(['/'])
+                    }
+                }
+            )
+    }
+
+    canActivateChild(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot
+    ): boolean | Observable<boolean> | Promise<boolean> {
+        return this.canActivate(route, state)
+    }
+}
